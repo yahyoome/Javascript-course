e0c06a60c174c5dde0965c47921847c0e80190ee
Yahyobek <caisophiya@gmail.com>
2024-10-30 10:21:46 +0500

    Javascript course is writing

---
+++ Ma'lumot-turlari/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// Number
+let number = 12; // Data Type = Number
+console.log(-8 / 0) // Infinity && -Infinity
+console.log('yahyo' * 12) // NaN = Not a Number: Son emas degan manoni bildiradi
+
+// <====================================================================================>
+
+// String
+let myName = 'Yahyobek' // string ni faqat qo'shtirnoq yoki birtirnoq yoki baksic ni ichiga yozishimiz kerak boladi
+
+// <====================================================================================>
+
+// Boolean
+let isMarried = false // yo'q || noto'g'ri
+let isCircleEarth = true // ha || to'g'ri   
+console.log(isMarried, isCircleEarth)
+
+// <====================================================================================>
+
+// Null
+// eslatma: null = bu yoq o'zgaruvchini ishlatsak bu null ga teng bo'ladi
+console.log(loveName)
+
+// <====================================================================================>
+
+// Undefined
+let whoIs;
+console.log(whoIs)    
+
+// <====================================================================================>
+
+// Object
+let thief = {
+    age: 30,
+    jacket: 'blue',
+    isLong: false,
+}    
---
+++ O'zgaruvchilar/script.js
@@ -0,0 +1,27 @@
+let number = 10
+const myName = 'Yahyo'
+console.log(number);
+
+number = 20
+console.log(number);
+
+// eslatma: o'zgaruvchini oldidan raqam yozish mumkin emas
+// va javascript ning kalit ya'ni key lar bilan nomlab qoyish ham mumkin emas
+
+// let alert = 'alert';
+// let 12Kun = '12';
+
+// ==========================================================================
+
+// let va const ning farqlari
+// const ni to'g'ridan to'g'ri o'zgartirish mumkin emas lekin o'zgartirsa bo'ladi
+
+// ==========================================================================
+
+// console.log(): nima? console.log() bu javascript yozgan code larimizni tekshirib beradi va karobkamizni ichida nima borligini ham tekshirib beradi
+console.log(myName)
+
+// ==========================================================================
+
+// Eskicha o'zgaruvchi yaratish
+// lkin var hozirgi kunda ishlatmelar
