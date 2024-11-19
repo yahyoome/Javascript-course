8aa7266fbfc99aac05c57fe3e5e9403e15686f4e
Yahyobek <caisophiya@gmail.com>
2024-11-19 13:41:08 +0500

    added new courses

---
+++ Method-va-Xususyatlar/script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// 1) length = ichidagi valyusi nechtaligini aniqlab beradi
+// eslatma = length string property hisoblanadi
+let myName = 'Yahyobek'
+console.log(myName.length)
+
+// 2) Method ni doyimp chaqirishimiz kerak bo'ladi: toUpperCase = toUpperCase()
--- Uyga-vazifa/script.js
+++ Uyga-vazifa/script.js
@@ -12,12 +12,27 @@ let seriesDB = {
 
 seriesDB['age'] = '20'
 
-let a = prompt("Oxirgi ko'rgan serailingiz?"),
-b = prompt("Necha baho berasiz?"),
-c = prompt("Oxirgi ko'rgan serailingiz?"),
-d = prompt("Necha baho berasiz?");
+function optimisition() {
+    if (seriesDB.count <= 5) {
+        console.log("Kam serial ko'ribsiz")
+    } else if (seriesDB.count > 5 && seriesDB.count < 10) {
+        console.log("Siz klassic tomoshabin ekansiz")
+    } else {
+        console.log("Siz serialchi zvezda ekansiz")
+    }
+}
+optimisition()
 
-seriesDB.series[a] = b // biza qachonki object ga hususiyat berarkanmiza unga . bilan emas [] kavs bilan beramiz agarda string yozmoqchi bo'lsak "" lar bilan yozamiz
-seriesDB.series[c] = d // biza qachonki object ga hususiyat berarkanmiza unga . bilan emas [] kavs bilan beramiz agarda string yozmoqchi bo'lsak "" lar bilan yozamiz
-
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Oxirgi ko'rgan serailingiz?"),
+    b = +prompt("Necha baho berasiz?");
+    
+    if (a != null && b != null & a != '' && b != '') {
+        seriesDB.series[a] = b // biza qachonki object ga hususiyat berarkanmiza unga . bilan emas [] kavs bilan beramiz agarda string yozmoqchi bo'lsak "" lar bilan yozamiz 
+        console.log('Done')
+    } else {
+        console.log('Error')
+        i--;
+    }
+}
 console.log(seriesDB)
