8c3b7a38bea830a4eee53b349d87b170e41e7576
Yahyobek <caisophiya@gmail.com>
2024-11-23 13:46:23 +0500

    Updated

--- Method-va-Xususyatlar/script.js
+++ Method-va-Xususyatlar/script.js
@@ -1,8 +1,12 @@
 'use strict'
 
+// String methods
+
 // 1) length = ichidagi valyusi nechtaligini aniqlab beradi
 // eslatma = length string property hisoblanadi
 let myName = 'Yahyobek'
 console.log(myName.length)
 
-// 2) Method ni doyimp chaqirishimiz kerak bo'ladi: toUpperCase = toUpperCase()
+// 2) Method ni doyim chaqirishimiz kerak bo'ladi: toUpperCase = toUpperCase()b=
+const myLove = 'Mubina'
+console.log(myLove.toUpperCase())
